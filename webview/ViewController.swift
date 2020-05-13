5769402fcab970665dabd96043a8ba55ab3bc7c2
Norat Saraswat <norat.company@gmail.com>
2020-05-13 08:34:56 +0530

    update Webview

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -15,18 +15,17 @@ class ViewController: UIViewController,WKNavigationDelegate {
         print("WebView Starting")
         webView = WKWebView(frame:  self.view.frame)
         webView!.navigationDelegate = self
+        // MARK:- Work with your original URL, some JS not working on webview html
         if let path = Bundle.main.url(forResource: "landingpage", withExtension: "html"){
             let myURLRequest:URLRequest = URLRequest(url: path)
             webView!.load(myURLRequest)
         }
         self.view.addSubview(webView!)
-        self.view.sendSubviewToBack(webView!)
-        
-        
     }
     
-    
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        /* this is for calling backtoapp js function if you get result in JSON then fine */
         webView.evaluateJavaScript("backtoapp()", completionHandler: { (result, error) in
             if error == nil {
                 if result != nil {
@@ -35,17 +34,12 @@ class ViewController: UIViewController,WKNavigationDelegate {
             } else {
                 print(error as Any)
             }
-            
         })
-        
+        //  Else you can get text for this div and can play with them
         webView.evaluateJavaScript("document.body.innerText") { (result, error) in
             print(result as Any)
         }
-        
     }
-    
-    
-    
 }
 
 
